b2c3ad6e1cda5e00420183677c7a8e1ff4d2083d
최승은 <liberalfilm@gmail.com>
2023-02-10 14:27:28 +0900

    2023-02-11-Fri-14:27

--- src/main/java/com/javaex/controller/MainController.java
+++ src/main/java/com/javaex/controller/MainController.java
@@ -1,4 +1,16 @@
 package com.javaex.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class MainController {
+
+    @RequestMapping("/")
+    public String mainPage() {
+        System.out.println("[MainController] > mainPage()");
+        return "main/index";
+    }
+
 }
